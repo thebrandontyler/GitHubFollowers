9df472ca2f16a3c37ecd6210a9c43b12eb167c48
Brandon Maynard <brandontyler@me.com>
2020-02-02 21:10:12 -0800

    Empty state view
    - Refactored custom views group for better organization
    - Implemented a reusable empty state view for users with no followers

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -59,6 +59,13 @@ class FollowerListVC: UIViewController {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them 😀."
+                    DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view) }
+                    return
+                }
+                
                 self.updateData()
                 
             case .failure(let error):
