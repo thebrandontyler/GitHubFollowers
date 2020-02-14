ff95494db23db01d5aba342b0c1e074e4e03f6a8
Brandon Maynard <brandontyler@me.com>
2020-02-13 16:42:59 -0800

    Child View Controller: UserInfoHeaderVC
    - Added GFSecondaryTitleLabel
    - Added GFUserInfoHeaderVC with image view and multiple labels
    - Added constants file with SFSymbols enum

---
+++ GHFollowers/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GHFollowers
+//
+//  Created by Brandon Tyler on 2/13/20.
+//  Copyright © 2020 Brandon Maynard. All rights reserved.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
