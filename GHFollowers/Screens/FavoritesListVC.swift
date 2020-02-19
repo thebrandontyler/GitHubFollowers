23b25ecd1a5750475c1bfecb8bb75c615000876a
Brandon Maynard <brandontyler@me.com>
2020-02-19 13:07:26 -0800

    UITableView Optimizations
    - Added extension for UITableView with functions for common use cases
    - Bug fix where cell is only removed if the data is removed from persistence

--- GHFollowers/Screens/FavoritesListVC.swift
+++ GHFollowers/Screens/FavoritesListVC.swift
@@ -32,11 +32,12 @@ class FavoritesListVC: GFDataLoadingVC {
     
     func configureTableView() {
         view.addSubview(tableView)
-        tableView.frame = view.bounds
         
+        tableView.frame = view.bounds
         tableView.rowHeight = 80
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.removeExcessCells()
         
         tableView.register(FavoriteCell.self, forCellReuseIdentifier: String(describing: FavoriteCell.self))
     }
@@ -87,13 +88,13 @@ extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete else { return }
         
-        let favorite = favorites[indexPath.row]
-        favorites.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .left)
-        
-        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+        PersistenceManager.updateWith(favorite: favorites[indexPath.row], actionType: .remove) { [weak self] error in
             guard let self = self else { return }
-            guard let error = error else { return }
+            guard let error = error else {
+                self.favorites.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .left)
+                return
+            }
             self.presentGFAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
         }
     }
