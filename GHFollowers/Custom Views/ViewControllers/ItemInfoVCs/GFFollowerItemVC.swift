1abf678955a216cd43cef177e773ef13e792db5d
Brandon Maynard <brandontyler@me.com>
2021-10-01 16:29:52 -0700

    New UIButton Configurations

--- GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -33,7 +33,7 @@ class GFFollowerItemVC: GFItemInfoVC {
     private func configureItems() {
         itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
         itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
-        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+        actionButton.set(color: .systemGreen, title: "Get Followers", systemImageName: "person.3")
     }
     
     override func actionButtonTapped() {
